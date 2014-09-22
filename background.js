7151c306a36e93a1f02969c35b3cc8b81a29acc2
Flor Kaucic <florkaucic@gmail.com>
2014-09-22 03:38:54 -0300

    Hello World Pop-up fix 2

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+chrome.browserAction.onClicked.addListener(function() {
+    var w = 440;
+    var h = 220;
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2); 
+
+    chrome.windows.create({'url': 'window.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
+    });
 });
